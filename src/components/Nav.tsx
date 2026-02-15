1b9c9b2e71fcc32d5bd4fce758451d67c6cf1aa6
Viet An <tiktaktictac269@gmail.com>
2026-02-15 09:50:35 +0700

    feat: add conditional elevation for navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,53 +2,56 @@
 import { Icon } from '@iconify/react';
 import { AppBar, Box, Button, Stack } from '@mui/material';
 import NextLink from 'next/link';
+import ElevationScroll from '@/src/components/ElevationScroll';
 import HomeLink from '@/src/components/HomeLink';
 import NavCategories from '@/src/components/NavCategories';
 
 export default function Nav() {
   return (
-    <AppBar
-      color="primary"
-      position="sticky"
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        py: 1,
-        px: 2,
-      }}
-    >
-      <Box
+    <ElevationScroll>
+      <AppBar
+        color="primary"
+        position="sticky"
         sx={{
           display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
           alignItems: 'center',
-          gap: 3,
+          py: 1,
+          px: 2,
         }}
       >
-        <HomeLink />
-        <NavCategories />
-      </Box>
-      <Stack direction="row" spacing={1}>
-        <Button
-          color="inherit"
-          component={NextLink}
-          href="/admin"
-          startIcon={
-            <Icon icon="material-symbols:admin-panel-settings-rounded" />
-          }
-          variant="outlined"
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 3,
+          }}
         >
-          Admin
-        </Button>
-        <Button
-          color="inherit"
-          startIcon={<Icon icon="material-symbols:person-rounded" />}
-          variant="outlined"
-        >
-          Login
-        </Button>
-      </Stack>
-    </AppBar>
+          <HomeLink />
+          <NavCategories />
+        </Box>
+        <Stack direction="row" spacing={1}>
+          <Button
+            color="inherit"
+            component={NextLink}
+            href="/admin"
+            startIcon={
+              <Icon icon="material-symbols:admin-panel-settings-rounded" />
+            }
+            variant="outlined"
+          >
+            Admin
+          </Button>
+          <Button
+            color="inherit"
+            startIcon={<Icon icon="material-symbols:person-rounded" />}
+            variant="outlined"
+          >
+            Login
+          </Button>
+        </Stack>
+      </AppBar>
+    </ElevationScroll>
   );
 }
